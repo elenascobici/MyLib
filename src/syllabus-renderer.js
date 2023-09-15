1b5181f276cd2988c9965f76da6ff35ea966d339
elenascobici <elena@scobici.com>
2023-09-14 22:36:13 -0400

    Added functionality for classes

--- src/syllabus-renderer.js
+++ src/syllabus-renderer.js
@@ -17,7 +17,7 @@ const modal = document.getElementById("taskmodal");
 const taskNameInput = document
     .getElementById('taskname');
 const typeInput = document.getElementById('typebutton');
-const classInput = document.getElementById('classbutton');
+const classInput = document.getElementById('class-button');
 const dueDateInput = document
     .getElementById('duedate');
 const plannedDateInput = document
@@ -62,15 +62,10 @@ closeButton.onclick = function () {
     modal.style.display = "none";
 };
 window.onclick = function (event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 };
-// Open the modal for editing when a row is clicked.
-Array.from(document.getElementsByClassName('syllabus-table-row'))
-    .forEach(row => {
-    row.addEventListener('click', rowClicked);
-});
 function rowClicked(e) {
     const eventTarget = e.target;
     if (!eventTarget.classList.contains('deletecell')) {
@@ -86,7 +81,7 @@ function rowClicked(e) {
         modal.style.display = "block";
     }
 }
-// Add event listener for mouse entering and leaving dropdown menu.
+// Add event listeners for mouse entering and leaving dropdown menu.
 Array.from(document.querySelectorAll('.dropdown')).forEach((element) => {
     const contentStyle = element.querySelector('.dropdown-content').style;
     element.addEventListener('mouseover', function () {
@@ -96,23 +91,129 @@ Array.from(document.querySelectorAll('.dropdown')).forEach((element) => {
         contentStyle.display = 'none';
     });
 });
-// Create event listener for dropdown option being clicked.
-document.querySelectorAll('.dropdownoption').forEach(option => {
-    option.addEventListener('click', (e) => {
-        var selectedElement = e.currentTarget;
-        selectedElement.parentElement.parentElement
-            .getElementsByClassName('dropbtn')[0]
-            .innerHTML = selectedElement.innerHTML;
-        selectedElement.parentElement.style.display = 'none';
-    });
-});
 // Load the table from the user data.
 function LoadTable() {
     data.syllabus.table.forEach((row) => {
         new Task(row.name, row.type, row.class, row.due, row.planned, row.status, row.phases);
     });
 }
+// Populate the dropdown list of classes from the user's json data.
+function PopulateClasses() {
+    data.syllabus.classes.forEach((className) => {
+        const classOption = document.createElement('a');
+        classOption.classList.add('dropdown-option');
+        classOption.innerHTML = className;
+        document.getElementById('class-content').insertBefore(classOption, document.getElementById('add-class'));
+    });
+}
+let oldClassName;
+// 
+function EditDropdownOption(e) {
+    const target = e.target;
+    const newClass = document.createElement('input');
+    newClass.classList.add('dropdown-option');
+    newClass.classList.add('textinput');
+    oldClassName = target.innerHTML;
+    newClass.value = oldClassName;
+    newClass.addEventListener('focus', function ListenForKeyPress() {
+        newClass.addEventListener('keypress', EditClassOption);
+        newClass.addEventListener('focusout', function () {
+            newClass.removeEventListener('keypress', EditClassOption);
+        });
+    });
+    document.getElementById('class-content').replaceChild(newClass, target);
+}
+// Helper event listener for editing a class option if the user hits the enter
+// key while editing a class input.
+function EditClassOption(e) {
+    if (e.key === 'Enter') {
+        const target = e.target;
+        const newClassName = target.value;
+        const newClassOption = document.createElement('a');
+        newClassOption.classList.add('dropdown-option');
+        newClassOption.addEventListener('click', DropdownOptionClick);
+        newClassOption.addEventListener('contextmenu', EditDropdownOption);
+        newClassOption.innerHTML = newClassName;
+        document.getElementById('class-content').replaceChild(newClassOption, target);
+        // If the edited class name is selected, change the selected option text.
+        const classDropdown = document.getElementById('class-button');
+        if (classDropdown.innerHTML == oldClassName) {
+            classDropdown.innerHTML = newClassName;
+        }
+        // Replace the class name in data.syllabus.classes.
+        const classArray = data.syllabus.classes;
+        classArray.splice(classArray.indexOf(oldClassName));
+        classArray.push(newClassName);
+        // Replace the class name in every existing task.
+        let rowIndex = 1;
+        data.syllabus.table.forEach((task) => {
+            if (task.class == oldClassName) {
+                task.class = newClassName;
+                document.getElementById('syllabus-table').rows[rowIndex].cells[2].innerHTML =
+                    newClassName;
+            }
+            rowIndex++;
+        });
+        GenericMethods.UpdateUserData();
+    }
+}
+// Helper event listener method for creating a new class option if the user
+// hits the enter key while focusing on a class input.
+function CreateClassOption(e) {
+    if (e.key === 'Enter') {
+        const target = e.target;
+        const newClassName = target.value;
+        // Don't allow empty class names.
+        if (newClassName === '') {
+            document.getElementById('class-content').removeChild(target);
+            return;
+        }
+        const newClassOption = document.createElement('a');
+        newClassOption.classList.add('dropdown-option');
+        newClassOption.addEventListener('click', DropdownOptionClick);
+        newClassOption.addEventListener('contextmenu', EditDropdownOption);
+        newClassOption.innerHTML = newClassName;
+        document.getElementById('class-content').replaceChild(newClassOption, target);
+        if (!data.syllabus.classes) {
+            data.syllabus.classes = [];
+        }
+        data.syllabus.classes.push(newClassName);
+        GenericMethods.UpdateUserData();
+        target.removeEventListener('keypress', CreateClassOption);
+    }
+}
+function DropdownOptionClick(e) {
+    var selectedElement = e.currentTarget;
+    if (selectedElement.id === 'add-class') {
+        const newClass = document.createElement('input');
+        newClass.classList.add('dropdown-option');
+        newClass.classList.add('textinput');
+        newClass.addEventListener('focus', function ListenForKeyPress() {
+            newClass.addEventListener('keypress', CreateClassOption);
+            newClass.addEventListener('focusout', function () {
+                newClass.removeEventListener('keypress', CreateClassOption);
+            });
+        });
+        document.getElementById('class-content').insertBefore(newClass, document.getElementById('add-class'));
+    }
+    else {
+        selectedElement.parentElement.parentElement
+            .getElementsByClassName('dropbtn')[0]
+            .innerHTML = selectedElement.innerHTML;
+        selectedElement.parentElement.style.display = 'none';
+    }
+}
 window.onload = function () {
     LoadTable();
+    // Open the modal for editing when a row is clicked.
+    Array.from(document.getElementsByClassName('syllabus-table-row'))
+        .forEach(row => {
+        row.addEventListener('click', rowClicked);
+    });
+    PopulateClasses();
+    document.querySelectorAll('.dropdown-option').forEach(option => {
+        option.addEventListener('click', DropdownOptionClick);
+        option.addEventListener('contextmenu', EditDropdownOption);
+    });
     SyllabusGenericMethods.ColorSettings(data.main.colorsetting);
 };
--- src/syllabus-renderer.ts
+++ src/syllabus-renderer.ts
@@ -19,7 +19,7 @@ const modal = document.getElementById("taskmodal")!;
 const taskNameInput: HTMLInputElement = document
     .getElementById('taskname')! as HTMLInputElement;
 const typeInput = document.getElementById('typebutton')!;
-const classInput = document.getElementById('classbutton')!; 
+const classInput = document.getElementById('class-button')!; 
 const dueDateInput: HTMLInputElement = document
     .getElementById('duedate')! as HTMLInputElement;
 const plannedDateInput: HTMLInputElement = document
@@ -64,24 +64,20 @@ createButton.onclick = function() {
     SyllabusGenericMethods.UpdateUserData();
 }
 
-// Close the modal when either the close button or gray area around the
-// modal are clicked.
+// Close the modal when either the close button is clicked.
 closeButton.onclick = function() {
     modal.style.display = "none";
 }
 
+// Close the modal if the user clicks outside of the modal.
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
 
-// Open the modal for editing when a row is clicked.
-Array.from(document.getElementsByClassName('syllabus-table-row'))
-    .forEach(row => {
-        row.addEventListener('click', rowClicked);
-});
-
+// Event listener method for displaying the editing modal when a row is
+// clicked.
 function rowClicked(e: Event) {
     const eventTarget: HTMLElement = e.target as HTMLElement;
     if (!eventTarget.classList.contains('deletecell')) {
@@ -98,7 +94,7 @@ function rowClicked(e: Event) {
     }
 }
 
-// Add event listener for mouse entering and leaving dropdown menu.
+// Add event listeners for mouse entering and leaving dropdown menu.
 Array.from(document.querySelectorAll('.dropdown')).forEach((element) => {
     const contentStyle = (element.querySelector('.dropdown-content')! as
         HTMLElement).style;
@@ -110,17 +106,6 @@ Array.from(document.querySelectorAll('.dropdown')).forEach((element) => {
     })
 })
 
-// Create event listener for dropdown option being clicked.
-document.querySelectorAll('.dropdownoption').forEach(option => {
-    option.addEventListener('click', (e: Event) => {
-        var selectedElement = e.currentTarget! as HTMLElement;
-        selectedElement.parentElement!.parentElement!
-            .getElementsByClassName('dropbtn')![0]
-            .innerHTML = selectedElement.innerHTML;
-        selectedElement.parentElement!.style.display = 'none';
-    });
-});
-
 // Load the table from the user data.
 function LoadTable() {
     data.syllabus.table.forEach((row: typeof Task) => {
@@ -129,7 +114,138 @@ function LoadTable() {
     })
 }
 
+// Populate the dropdown list of classes from the user's json data.
+function PopulateClasses() {
+    data.syllabus.classes.forEach((className: string) => {
+        const classOption = document.createElement('a');
+        classOption.classList.add('dropdown-option');
+        classOption.innerHTML = className;
+        document.getElementById('class-content')!.insertBefore(
+            classOption, document.getElementById('add-class'));
+    })
+}
+
+let oldClassName: string;
+
+// Event listener method for editing a class dropdown option when it is right
+// clicked.
+function EditDropdownOption(e: Event) {
+    const target = e.target! as HTMLElement;
+    const newClass = document.createElement('input');
+    newClass.classList.add('dropdown-option');
+    newClass.classList.add('textinput');
+    oldClassName = target.innerHTML;
+    newClass.value = oldClassName;
+    newClass.addEventListener('focus', function ListenForKeyPress() {
+        newClass.addEventListener('keypress', EditClassOption);
+        newClass.addEventListener('focusout', function() {
+            newClass.removeEventListener('keypress', EditClassOption);
+        });
+    });
+    document.getElementById('class-content')!.replaceChild(newClass, target);
+}
+
+// Helper event listener for editing a class option if the user hits the enter
+// key while editing a class input.
+function EditClassOption(e: Event) {
+    if ((e as KeyboardEvent).key === 'Enter') {
+        const target = e.target! as HTMLInputElement;
+        const newClassName = target.value;
+        const newClassOption = document.createElement('a');
+        newClassOption.classList.add('dropdown-option');
+        newClassOption.addEventListener('click', DropdownOptionClick);
+        newClassOption.addEventListener('contextmenu', EditDropdownOption);
+        newClassOption.innerHTML = newClassName;
+        document.getElementById('class-content')!.replaceChild(newClassOption,
+        target);
+        // If the edited class name is selected, change the selected option
+        // text.
+        const classDropdown = document.getElementById('class-button')!;
+        if (classDropdown.innerHTML == oldClassName) {
+            classDropdown.innerHTML = newClassName;
+        }
+        // Replace the class name in data.syllabus.classes.
+        const classArray = data.syllabus.classes;
+        classArray.splice(classArray.indexOf(oldClassName));
+        classArray.push(newClassName);
+        // Replace the class name in every existing task.
+        let rowIndex = 1;
+        data.syllabus.table.forEach((task: typeof Task) => {
+            if (task.class == oldClassName) {
+                task.class = newClassName;
+                (document.getElementById('syllabus-table')! as 
+                    HTMLTableElement).rows[rowIndex].cells[2].innerHTML = 
+                    newClassName;
+            }
+            rowIndex++;
+        })
+        GenericMethods.UpdateUserData();
+    }
+}
+
+// Helper event listener method for creating a new class option if the user
+// hits the enter key while focusing on a class input.
+function CreateClassOption(e: Event) {
+    if ((e as KeyboardEvent).key === 'Enter') {
+        const target = e.target! as HTMLInputElement;
+        const newClassName = target.value;
+        // Don't allow empty class names.
+        if (newClassName === '') {
+            document.getElementById('class-content')!.removeChild(target);
+            return;
+        }
+        const newClassOption = document.createElement('a');
+        newClassOption.classList.add('dropdown-option');
+        newClassOption.addEventListener('click', DropdownOptionClick);
+        newClassOption.addEventListener('contextmenu', EditDropdownOption);
+        newClassOption.innerHTML = newClassName;
+        document.getElementById('class-content')!.replaceChild(newClassOption,
+        target);
+        if (!data.syllabus.classes) {
+            data.syllabus.classes = [];
+        }
+        data.syllabus.classes.push(newClassName);
+        GenericMethods.UpdateUserData();
+        target.removeEventListener('keypress', CreateClassOption);
+    }
+}
+
+// Event listener method for dropdown option being clicked.
+function DropdownOptionClick (e: Event) {
+    var selectedElement = e.currentTarget! as HTMLElement;
+    // In case that user is adding a class.
+    if (selectedElement.id === 'add-class') {
+        const newClass = document.createElement('input');
+        newClass.classList.add('dropdown-option');
+        newClass.classList.add('textinput');
+        newClass.addEventListener('focus', function ListenForKeyPress() {
+            newClass.addEventListener('keypress', CreateClassOption);
+            newClass.addEventListener('focusout', function() {
+                newClass.removeEventListener('keypress', CreateClassOption);
+            });
+        });
+        document.getElementById('class-content')!.insertBefore(newClass,
+            document.getElementById('add-class'));
+    }
+    else {
+        selectedElement.parentElement!.parentElement!
+            .getElementsByClassName('dropbtn')![0]
+            .innerHTML = selectedElement.innerHTML;
+        selectedElement.parentElement!.style.display = 'none';
+    }
+}
+
 window.onload = function() {
     LoadTable();
+    // Open the modal for editing when a row is clicked.
+    Array.from(document.getElementsByClassName('syllabus-table-row'))
+    .forEach(row => {
+        row.addEventListener('click', rowClicked);
+    });
+    PopulateClasses();
+    document.querySelectorAll('.dropdown-option').forEach(option => {
+        option.addEventListener('click', DropdownOptionClick);
+        option.addEventListener('contextmenu', EditDropdownOption);
+    });
     SyllabusGenericMethods.ColorSettings(data.main.colorsetting);
 }
